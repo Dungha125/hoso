51db13f0783994df691fadb53fbb8c5a5586fa18
Dungha125 <dungha122405@gmail.com>
2023-05-08 01:24:56 +0700

    dungha

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const image = document.querySelector("img")
-const input = document.querySelector("input")
+// Lấy đối tượng input và img
+var inputImage = document.getElementById("inputImage");
+var previewImage = document.getElementById("previewImage");
 
-input.addEventListener("change", () => {
-    image.src = URL.createObjectURL(input.file[0])
-})
+// Thiết lập sự kiện thay đổi cho input
+inputImage.addEventListener("change", function() {
+  // Kiểm tra xem người dùng đã chọn ảnh chưa
+  if (inputImage.files && inputImage.files[0]) {
+    // Tạo một đối tượng FileReader để đọc file ảnh
+    var reader = new FileReader();
+
+    // Thiết lập sự kiện load để cập nhật hình ảnh trên trang web
+    reader.onload = function(e) {
+      previewImage.src = e.target.result;
+    }
+
+    // Đọc file ảnh và hiển thị lên trang web
+    reader.readAsDataURL(inputImage.files[0]);
+  }
+});
